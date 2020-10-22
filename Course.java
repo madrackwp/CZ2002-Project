c7e28c9f2a6848b19a6becd0b01482de81c780c6
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-10-22 15:50:06 +0800

    Added files

--- Course.java
+++ Course.java
@@ -1,9 +1,13 @@
 import java.util.*;
 
 public class Course {
-    private String courseCode, school, type;
-    private int vacancies, indexNo, totalCapacity;
-    private ArrayList<Student> students;
-    private ArrayList<Student> waitList;
+    private String courseCode, school, moduleType;
+    private ArrayList<CourseIndex> indexes;
 
+    public Course() {
+    };
+
+    public boolean editCourse() {
+        return true;
+    }
 }
