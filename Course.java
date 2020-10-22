0809822d7c347f6aec55cb382f954b518762109f
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-10-22 16:48:18 +0800

    Still doing course

--- Course.java
+++ Course.java
@@ -1,13 +1,34 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Course {
-    private String courseCode, school, moduleType;
+    private String courseCode, school;
     private ArrayList<CourseIndex> indexes;
 
-    public Course() {
+
+    public Course(String courseCode, String school) {
+        this.courseCode = courseCode;
+        this.school = school;
+        this.indexes = new ArrayList<CourseIndex>();
     };
 
-    public boolean editCourse() {
-        return true;
+
+    public boolean editCourse(String courseCode){
+
     }
+
+    public String printIndexes(){}
+
+    public boolean createCourse(){}
+
+    public boolean deleteCourse(){}
+
+    public boolean editCourse(){}
+
+    public boolean checkCourseAvail(){}
+
+    public void printStudentsByIndex(){}
+
+    public void printStudentsByCourse(){} 
+
+   public
 }
