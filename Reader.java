bbec28400971e6ebb9ee6b0e3f5bac3b2c756042
Chong Jing Hong <congjinghong@gmail.com>
2020-11-12 15:16:59 +0800

    edited a lot of things, settled login for users

---
+++ Reader.java
@@ -0,0 +1,169 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Reader {
+	
+	public Reader () {}
+
+	//Student stuff
+	public void studentLogin () {
+		String line;
+		boolean userBool = false;
+
+		//read input
+		System.out.print("Enter student username: ");
+		Scanner sc = new Scanner(System.in);
+		String userID = sc.next();
+		System.out.print("Enter student password: ");
+		int passID = sc.next().hashCode();
+		String hashedPassword = Integer.toString(passID);
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("studentdata.txt"));
+			do {
+				line = reader.readLine();
+				String [] tokens = line.split(" ");
+				if ((tokens[0].equals(userID)) && tokens[1].equals(hashedPassword)) {
+					System.out.println("Login Successful");
+					break;
+				}
+				else
+					if (tokens[0].equals(userID))
+						userBool = true;
+			} while (line != null);
+			reader.close();
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		catch (NullPointerException n) {
+			// null exception handle by contain() function
+			// for when account don't match
+			if (userBool == false)
+				System.out.println("Invalid Username");
+			else
+				System.out.println("Invalid Password");
+		}
+	}
+
+	public String getSchool(String username) {
+		String line;
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("studentdata.txt"));
+			do {
+				line = reader.readLine();
+				String [] tokens = line.split(" ");
+				if (tokens[0].equals(username)) {
+					reader.close();
+					return tokens[3];
+				}
+			} while (line != null);
+			reader.close();
+			return "";
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+			return "";
+		}
+		catch (NullPointerException n) {
+			System.out.println("Username not found");
+			return "";
+		}
+	}
+
+	//	public void getCourseInformation (String course) {
+//		String line;
+//		try {
+//			BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\bghx9\\eclipse-workspace\\STARS\\course.txt"));
+//			do {
+//				line = reader.readLine();
+//				String [] tokens = line.split(" ");
+//				if (course.equals(tokens[0])) {
+//					System.out.println("Index: " + tokens[1]);
+//					System.out.println("Total: " + tokens[2]);
+//					System.out.println("Vacancy: " + tokens[3]);
+//					System.out.println("Waitlist: " + tokens[4]);
+//				}
+//			} while (line != null);
+//			reader.close();
+//		}
+//		catch (IOException e) {
+//			System.out.println("An error occurred.");
+//			e.printStackTrace();
+//		}
+//		catch (NullPointerException n) {}
+//	}
+//
+//	public int getVacancy (String course, String index) {
+//		String line;
+//		try {
+//			BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\bghx9\\eclipse-workspace\\STARS\\course.txt"));
+//			do {
+//				line = reader.readLine();
+//				String [] tokens = line.split(" ");
+//				if (course.equals(tokens[0]) && index.equals(tokens[1])) {
+//					reader.close();
+//					return Integer.parseInt(tokens[3]);
+//				}
+//			} while (line != null);
+//			reader.close();
+//			return 0;
+//		}
+//		catch (IOException e) {
+//			System.out.println("An error occurred.");
+//			e.printStackTrace();
+//			return 0;
+//		}
+//		catch (NullPointerException n) {
+//			System.out.println("Invalid Course or Index");
+//			return 0;
+//		}
+//	}
+
+
+	//Staff stuff
+	public void staffLogin () {
+		String line;
+		boolean userBool = false;
+
+		//read input
+		System.out.print("Enter staff username: ");
+		Scanner sc = new Scanner(System.in);
+		String userID = sc.next();
+		System.out.print("Enter staff password: ");
+		int passID = sc.next().hashCode();
+		String hashedPassword = Integer.toString(passID);
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("staffdata.txt"));
+			do {
+				line = reader.readLine();
+				String [] tokens = line.split(" ");
+				if ((tokens[0].equals(userID)) && tokens[1].equals(hashedPassword)) {
+					System.out.println("Login Successful");
+					break;
+				}
+				else
+				if (tokens[0].equals(userID))
+					userBool = true;
+			} while (line != null);
+			reader.close();
+		}
+		catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		catch (NullPointerException n) {
+			// null exception handle by contain() function
+			// for when account don't match
+			if (userBool == false)
+				System.out.println("Invalid Username");
+			else
+				System.out.println("Invalid Password");
+		}
+	}
+
+}
