d0e1d3a160e8aab69b10b364b5a86328feb2810d
Chong Jing Hong <congjinghong@gmail.com>
2020-11-25 10:06:04 +0800

    added javadoc comments in UserLogin

--- Login/UserLogin.java
+++ Login/UserLogin.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 
 import Users.UserAcc;
 
+/**
+ * This interface is used for abstraction for users to login
+ * @author Chong Jing Hong
+ * @version 1.0
+ * @since 25/11/2020
+ */
+
 public interface UserLogin {
+    /**
+     * This method allows users, students and staff, to login to the STARS system
+     * @param users is the domain of login
+     * @return the user object
+     * @since 1.0
+     */
     public abstract Object login(Object users);
 }
+
