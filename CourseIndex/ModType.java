f8f2aa684d7716084f1e1349a969d184d007a9aa
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-11-25 11:45:08 +0800

    Done with java doc for CourseIndex

--- CourseIndex/ModType.java
+++ CourseIndex/ModType.java
@@ -1,5 +1,12 @@
 package CourseIndex;
 
+/**
+ * All allowed mod types in STARS\
+ * 
+ * @author Goh Wei Pin
+ * @version 1.0
+ * @since 2020-11-25
+ */
 public enum ModType {
     CORE, UE, GERPE_LA, GERPE_BM, GERPE_STS, MPE
 }
